e224e09d66ee1805ea61940fef5a1bef61420929
farhan <farhan@farhan-System-Product-Name>
2020-04-08 20:38:22 +0600

    Linked List completed (Stable Release)

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MENU printf("\nPress 1 to insert at head\nPress 2 to insert at middle\nPress 3 to insert at end\nPress 4 to print\nPress any other button to exit\n");
+#define MENU printf("\nPress 1 to insert at head\nPress 2 to insert at middle\nPress 3 to insert at end\nPress 4 to delete the head\nPress 5 to delete at middle\nPress 6 to delete end node\nPress 7 to update\nPress 8 to print\nPress any other button to exit\n");
 #define CHOICE printf("\nEnter Choice: ");
 #define VALUE printf("\nEnter value: ");
 
@@ -69,6 +69,43 @@ void insertAtEnd(Node *head, int data) {
 	insertAtEnd(head->next, data);
 }
 
+void update(Node *head, int position, int index, int data) {
+	if (index == 0) head->x = data;
+	else if (index == -1) {
+		if (head->next == NULL) {
+			head->x = data;
+			return;
+		}
+		update(head->next, position, index, data);
+	} else {
+		if (position == index) {
+			head->x = data;
+			return;
+		}
+		update(head->next, position + 1, index, data);
+	}
+}
+
+void deleteAtMiddle(Node *head, int position, int index) {
+	if (position == index - 1) {
+		Node *tmp;
+		tmp = head->next;
+		head->next = head->next->next;
+		free(tmp);
+		return;
+	}
+	deleteAtMiddle(head->next, position + 1, index);
+}
+
+void deleteEnd(Node *head) {
+	if (head->next->next == NULL) {
+		free(head->next);
+		head->next = NULL;
+		return;
+	}
+	deleteEnd(head->next);
+}
+
 int main(int argc, char** argv) {
 	Node *head;
 	int input, x;
@@ -88,6 +125,10 @@ int main(int argc, char** argv) {
 			head->next = NULL;
 			goto loop;
 		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
 			printf("List is empty!!!\n");
 			goto label;
 		default:
@@ -106,6 +147,10 @@ int main(int argc, char** argv) {
 			head = insertAtHead(head, x);
 			goto loop;
 		case 2:
+			if (head->next == NULL) {
+				printf("\nMiddle is undefined right now!!!");
+				goto loop;
+			}
 			printf("\nEnter position: ");
 			int index;
 			scanf("%d", &index);
@@ -122,6 +167,40 @@ int main(int argc, char** argv) {
 			insertAtEnd(head, x);
 			goto loop;
 		case 4:
+			if (head->next == NULL) {
+				free(head);
+				goto label;
+			}
+			Node *tmp;
+			tmp = head;
+			head = head->next;
+			free(tmp);
+			goto loop;
+		case 5:
+			if (head->next == NULL) {
+				printf("\nMiddle is undefined right now!!! So, deleting the head!");
+				free(head);
+				goto label;
+			}
+			printf("Enter position: ");
+			scanf("%d", &index);
+			deleteAtMiddle(head, 0, index);
+			goto loop;
+		case 6:
+			if (head->next == NULL) {
+				free(head);
+				goto label;
+			}
+			deleteEnd(head);
+			goto loop;
+		case 7:
+			printf("\nEnter position (-1 to update the last data): ");
+			scanf("%d", &index);
+			VALUE
+			scanf("%d", &x);
+			update(head, 0, index, x);
+			goto loop;
+		case 8:
 			printf("The list contains: ");
 			printList(head);
 			goto loop;
